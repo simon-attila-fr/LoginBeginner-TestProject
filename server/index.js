d576bd85de701d6514bc05cee11348d4507cbb72
Attila Simon <attila@mbp-de-ods-2.t.hu>
2023-01-04 16:15:37 +0100

    Register route added with hashed password

--- server/index.js
+++ server/index.js
@@ -2,9 +2,13 @@ require('dotenv').config()
 
 const express = require("express");
 const mysql = require("mysql2/promise");
+const cors = require("cors");
 
 const app = express();
 const port = parseInt(process.env.APP_PORT ?? "5000", 10);
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 app.use(express.json());
 
@@ -27,10 +31,47 @@ db
         console.error(err);
     })
 
+// TODO error handling: check if username already exists
+// TODO input check Joi
+
+app.get("/users", (req, res) => {
+    db
+        .query("SELECT id, username FROM users")
+        .then(([users]) => {
+            res.json(users);
+        })
+        .catch(((err) => {
+            res.status(500).send("Error while retrieving data from database.");
+        }))
+})
+
+app.post("/register", async (req, res) => {
+    const { username, password } = req.body;
+
+    const hashPassword = (plainPassword, options) => {
+        return bcrypt.hash(plainPassword, options);
+    };
+
+    const hashedPassword = await hashPassword(password, saltRounds);
+    db
+        .query(
+            "INSERT INTO users (username, password) VALUES (?,?)",
+            [username, hashedPassword],
+        )
+        .then((result) => {
+            const id = result.insertId;
+            res.status(200).send(`User has been successfully created with insertId ${id}.`);
+        })
+        .catch(((err) => {
+            // console.error(err);
+            res.status(500).send("Error while creating user.");
+        }))
+});
+
 app.listen(port, (err) => {
     if (err) {
-      console.error("Something bad happened.");
+        console.error("Something bad happened.");
     } else {
-      console.log("The Server is listening on the port that has been defined in your env file.");
+        console.log("The Server is listening on the port that has been defined in your env file.");
     }
-  });
+});
