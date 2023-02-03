1c5f46bee6536e905f16f57f2e8e505e3cd9d210
Attila Simon <simon.attila.fr@gmail.com>
2023-02-04 00:23:26 +0100

    Back: email added to auth process; reg process upgraded - if email exists

--- server/data/database.sql
+++ server/data/database.sql
@@ -1,5 +1,8 @@
-CREATE TABLE `users` (
-    `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `username` VARCHAR(255) UNIQUE NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    `isadmin` VARCHAR(1) NOT NULL);
+CREATE TABLE
+    `users` (
+        `id` INT PRIMARY KEY AUTO_INCREMENT,
+        `email` VARCHAR(255) UNIQUE NOT NULL,
+        `username` VARCHAR(255) UNIQUE NOT NULL,
+        `password` VARCHAR(255) NOT NULL,
+        `isadmin` VARCHAR(1) NOT NULL
+    );
--- server/validators/registerValidator.js
+++ server/validators/registerValidator.js
@@ -1,5 +1,7 @@
 const Joi = require('joi');
 const registerSchema = Joi.object({
+    email: Joi.string()
+        .email({ minDomainSegments: 2 }),
     username: Joi.string()
         .alphanum()
         .min(3)
