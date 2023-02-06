40af6226c69f6df02ddfe3f26afe25d21ec86340
Attila Simon <simon.attila.fr@gmail.com>
2023-02-06 13:59:07 +0100

    Sign In: first version with auth context

---
+++ client/src/components/SignIn.jsx
@@ -0,0 +1,99 @@
+import { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import axios from "../api/axios";
+
+const SIGNIN_URL = "/login";
+
+const SignIn = () => {
+  const { auth, setAuth } = useContext(AuthContext);
+  const emailRef = useRef();
+  const errorRef = useRef();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrorMsg("");
+  }, [email, password]);
+
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        SIGNIN_URL,
+        JSON.stringify({ email, password }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
+        }
+      );
+      setAuth({
+        ...auth,
+        username: response.data.user,
+        loggedIn: true,
+        message: response.data.message,
+        })
+        console.log(auth);
+        console.log(JSON.stringify(response));
+        console.log(response.data.accessToken);
+      setEmail("");
+      setPassword("");
+      setSuccess(true);
+    } catch (error) {}
+  };
+
+  return (
+    <>
+      {success ? (
+        <section>
+          <h1>{auth.message}</h1>
+        </section>
+      ) : (
+        <section>
+          <p ref={errorRef} aria-live="assertive">
+            {errorMsg}
+          </p>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSignIn}>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              ref={emailRef}
+              id="email"
+              autoComplete="off"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              autoComplete="off"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              required
+            />
+            <button>Sign In</button>
+            <p>
+              Don't already registered?
+              <br />
+              <span>
+                {/*Router link*/}
+                <a href="#">Sign Up</a>
+              </span>
+            </p>
+          </form>
+        </section>
+      )}
+    </>
+  );
+};
+
+export default SignIn;
